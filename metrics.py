3b3624b8ccaa9dedd9f47728007ecee68bef6dc4
jplumail <jeanplumail76@gmail.com>
2022-03-25 19:14:15 +0100

    clean up repo

--- metrics.py
+++ metrics.py
@@ -6,10 +6,26 @@ from tqdm import tqdm
 import rasterio as rio
 import numpy as np
 import torch
+from torch.utils.data import Subset
+import argparse
 
 from floatingobjects.data import FloatingSeaObjectDataset
 from floatingobjects.model import get_model
 
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data-path', type=str, default="/data")
+    parser.add_argument('--snapshot-path', type=str)
+    parser.add_argument('--metrics-path', type=str)
+    parser.add_argument('--test-negative-patches', action="store_true")
+    parser.add_argument('--workers', type=int, default=0)
+    parser.add_argument('--model', type=str, default="unet")
+    parser.add_argument('--image-size', type=int, default=128)
+    parser.add_argument('--device', type=str, choices=["cpu", "cuda"], default="cuda")
+    args = parser.parse_args()
+
+    return args
 
 def get_predictions(path):
     with rio.open(path) as src:
@@ -36,110 +52,82 @@ def get_metrics(confusion_matrix):
         out[c, 0] = tp / (fp + tp) # Precision
         out[c, 1] = tp / (fn + tp) # Recall
         out[c, 2] = 2 / (1/out[c,0] + 1/out[c,1]) # F1-score
-    return out
+    return out  
+                  
 
-
-def predictions_all(root_data, predictions_path):
-
-    for model in ["unet", "manet"]:
-        model_path = os.path.join(predictions_path, model)
-
-        for fold in [1, 2]:
-            fold_path = os.path.join(model_path, str(fold))
-            ds = FloatingSeaObjectDataset(root_data, fold="test", foldn=fold, hard_negative_mining=False, use_l2a_probability=0)
-
-            for classifier in ["no-classifier", "classifier"]:
-                classifier_path = os.path.join(fold_path, classifier)
-                os.makedirs(classifier_path, exist_ok=True)
-
-                cm = np.zeros((2,2), dtype=int)
-                # Iterate over regions
-                for dataset in tqdm(ds.datasets):
-                    # Get predictions
-                    region_path = os.path.join(classifier_path, f"{dataset.region}.tif")
-                    y_pred = get_predictions(region_path)
-
-                    # Get labels
-                    labels = get_labels(dataset, y_pred)
-
-                    t = 0.3
-                    y_pred = y_pred.ravel() > t
-                    labels = labels.ravel()
-
-                    # Compute the confusion matrix
-                    cm += np.bincount(labels * 2 + y_pred, minlength=4).reshape(2, 2).astype(int)
-                
-                yield model, fold, classifier, cm
-                
-                
-                
-
-def predictions_esa(root_data, predictions_path):
+def main(args):
     threshold = 0.03
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = args.device
     
-    for model in ["unet", "manet"]:
-        for fold in [1, 2]:
+    for fold in [1, 2]:
 
-            dataset = FloatingSeaObjectDataset(root_data, fold="test", foldn=fold, transform=None, output_size=128, use_l2a_probability=0.5)
-            loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=4)
+        dataset = FloatingSeaObjectDataset(
+            args.data_path, fold="test", foldn=fold, transform=None, output_size=args.image_size, use_l2a_probability=0,
+            hard_negative_mining=False
+        )
 
-            m = get_model(model, pretrained=False)
-            m.load_state_dict(torch.load(f"models\\{model}-posweight1-lr001-bs160-ep50-aug1-seed{fold-1}.pth.tar")["model_state_dict"])
-            m = m.to(device)
-            m.eval()
+        if args.test_negative_patches:
+            indices = dataset.filter_hnm() # indices of negatives patches only
+            dataset = Subset(dataset, indices)
+        
+        loader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=False, num_workers=args.num_workers)
 
-            for classifier in ["classifier", "no-classifier"]:
+        m = get_model(args.model, pretrained=False)
+        m.load_state_dict(torch.load(args.snapshot_path)["model_state_dict"])
+        m = m.to(device)
+        m.eval()
 
-                classifier_model_path = f"models\\checkpoint-fold{fold}.pt" if classifier=="classifier" else None
-                # Load classifier
-                if classifier_model_path:
-                    classifier_model = get_model("classifier")
-                    checkpoint = torch.load(classifier_model_path)
-                    classifier_model.load_state_dict(checkpoint["state_dict"])
-                    classifier_model = classifier_model.to(device)
-                    classifier_model.eval()
-                    center = checkpoint["center"].to(device)
-                    scale = checkpoint["scale"].to(device)
-                else:
-                    classifier_model = None
+        for classifier in ["no-classifier", "classifier"]:
 
-                cm = np.zeros((2,2), dtype=int)
-                for idx, (image, target, _) in tqdm(enumerate(loader), total=len(loader)):
-                    image = image.to(device, dtype=torch.float)
-                    image *= 1e-4
-                    with torch.no_grad():
-                        # forward pass: compute predicted outputs by passing inputs to the model
-                        logits = m.forward(image)
-                        output = torch.sigmoid(logits)[:, 0]
+            classifier_model_path = f"models\\checkpoint-fold{fold}.pt" if classifier=="classifier" else None
+            # Load classifier
+            if classifier_model_path:
+                classifier_model = get_model("classifier")
+                checkpoint = torch.load(classifier_model_path)
+                classifier_model.load_state_dict(checkpoint["state_dict"])
+                classifier_model = classifier_model.to(device)
+                classifier_model.eval()
+                center = checkpoint["center"].to(device)
+                scale = checkpoint["scale"].to(device)
+            else:
+                classifier_model = None
 
-                        if classifier == "classifier":
-                            mask_floating_objects = output > 0.05
-                            # Image transformation
-                            image *= 1e4
-                            image -= center[:,None,None]
-                            image /= scale[:,None,None]
+            cm = np.zeros((2,2), dtype=int)
+            for idx, (image, target, _) in tqdm(enumerate(loader), total=len(loader)):
+                image = image.to(device, dtype=torch.float)
+                image *= 1e-4
+                with torch.no_grad():
+                    # forward pass: compute predicted outputs by passing inputs to the model
+                    logits = m.forward(image)
+                    output = torch.sigmoid(logits)[:, 0]
 
-                            # Apply sliding windows
-                            classifier_scores = classifier_model.sliding_windows(image, mask_floating_objects)
-                            mask_ship = classifier_scores > 5e-4
-                            output[mask_ship] = 0.
+                    if classifier == "classifier":
+                        # Image transformation
+                        image *= 1e4
+                        image -= center[:,None,None]
+                        image /= scale[:,None,None]
 
-                        y = (output > threshold).cpu().view(-1).numpy()
-                        y_true = target.cpu().view(-1).numpy().astype(bool)
-                        cm += np.bincount(y_true * 2 + y, minlength=4).reshape(2, 2).astype(int)
-                
-                yield model, fold, classifier, cm
+                        # Apply sliding windows
+                        classifier_scores = classifier_model.sliding_windows(image, stride=8)
+                        mask_ship = classifier_scores > 0.99
+                        output[mask_ship] = 0.
+
+                    y = (output > threshold).cpu().view(-1).numpy()
+                    y_true = target.cpu().view(-1).numpy().astype(bool)
+                    cm += np.bincount(y_true * 2 + y, minlength=4).reshape(2, 2).astype(int)
+            
+            yield model, fold, classifier, cm
 
 
 
 if __name__ == "__main__":
+    args = parse_args()
 
-    root_data = "floatingobjects\\data"
-    predictions_path = "predictions"
+    metrics_path = args.metrics_path
+    os.makedirs(os.path.dirname(metrics_path), exist_ok=True)
 
-    for model, fold, classifier, cm in predictions_esa(root_data, predictions_path):
-        with open(os.path.join("predictions-esa", "metrics.txt"), "a") as f:
+    for model, fold, classifier, cm in main(args):
+        with open(os.path.join(metrics_path, "predictions-nohnm3.txt"), "a") as f:
             f.write(f"Model {model}, fold n°{fold}, {classifier}\n")
             f.write("Confusion matrix :\n")
 
@@ -151,6 +139,8 @@ if __name__ == "__main__":
 
             # Output confusion matrix
             f.write(repr(cm)+"\n")
+
+            f.write(f"\nfalse positive rate : {cm[0,1] / (cm[0,0]+cm[0,1]):.3f}")
 
             # Output Precision/recall/F1-score for water and floating objects
             metrics = get_metrics(cm)
